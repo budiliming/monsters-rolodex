e3d4f4a47b21efbe198948b26643a96146d1af20
Budi Liming Sanjaya <budi.sanjaya@ocbcnisp.com>
2020-01-27 16:13:27 +0700

    Updates

---
+++ precache-manifest.70816f9f3fa25429c0955855168241d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d8f449cc8a919b8c1dc2ba45394972",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "561df03be7faa2c9ae17",
+    "url": "/monsters-rolodex/static/css/main.348bf2ca.chunk.css"
+  },
+  {
+    "revision": "97d9334b8c908c1c647d",
+    "url": "/monsters-rolodex/static/js/2.4951d946.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.4951d946.chunk.js.LICENSE"
+  },
+  {
+    "revision": "561df03be7faa2c9ae17",
+    "url": "/monsters-rolodex/static/js/main.a5991c26.chunk.js"
+  },
+  {
+    "revision": "0725d3343eded53892b1",
+    "url": "/monsters-rolodex/static/js/runtime-main.ecd1d7fc.js"
+  }
+]);
